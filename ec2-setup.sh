dff960f7e53ca2df559771a1f4a2b9d2b42c2058
Quinlan Jung <quin@expo.io>
2022-06-01 14:25:44 -0700

    build project:

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -5,6 +5,8 @@ sudo yum -y install git # to pull down the repo
 sudo yum -y install gcc
 git clone https://github.com/quinlanj/nfsv4-locktest.git
 mv nfsv4-locktest locks
+cd locks
+make
 
 # only needed on the test server machine
 mkdir ~/secrets
